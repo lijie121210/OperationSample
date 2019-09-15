02db64cb8d8a9853b902a6929438b017c297fc2b
viwii <lijie121210@163.com>
2019-09-15 15:46:37 +0800

    complete

--- Sources/OperationSample/Operations/LocationOperation.swift
+++ Sources/OperationSample/Operations/LocationOperation.swift
@@ -26,11 +26,56 @@ open class LocationOperation: AnyOperation, CLLocationManagerDelegate {
     
     // MARK: Initialization
     
-    init(accuracy: CLLocationAccuracy, handler: @escaping (CLLocation) -> Void) {
+    public init(accuracy: CLLocationAccuracy, handler: @escaping (CLLocation) -> Void) {
         self.accuracy = accuracy
         self.handler = handler
+        
         super.init()
         
+        addCondition(LocationCondition(usage: .whenInUse))
+        addCondition(MutuallyExclusive<CLLocationManager>())
+    }
+    
+    open override func execute() {
+        DispatchQueue.main.async {
+            /*
+                `CLLocationManager` needs to be created on a thread with an active
+                run loop, so for simplicity we do this on the main queue.
+            */
+            let manager = CLLocationManager()
+            manager.desiredAccuracy = self.accuracy
+            manager.delegate = self
+            manager.startUpdatingLocation()
+            
+            self.manager = manager
+        }
+    }
+    
+    open override func cancel() {
+        DispatchQueue.main.async {
+            self.stopLocationUpdates()
+            super.cancel()
+        }
+    }
+    
+    private func stopLocationUpdates() {
+        manager?.stopUpdatingLocation()
+        manager = nil
+    }
+    
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last(where: { $0.horizontalAccuracy <= accuracy }) else {
+            return
+        }
+        
+        stopLocationUpdates()
+        handler(location)
+        finish()
+    }
+    
+    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        stopLocationUpdates()
+        finish(with: error as NSError)
     }
 }
 
